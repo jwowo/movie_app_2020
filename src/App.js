52b67a75be71944164f06a69fac56e264c3c81df
Jongwoo Han <jongwoo0221@gmail.com>
2021-05-18 01:47:13 +0900

    Finshed chapter 03 exercise.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from 'react';
+
+function Food({ fav }) {
+  return <h1>I like { fav } !!!</h1>;
+}
 
 function App() {
-  return <div className="App" />
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Food fav="KimChi1" />
+      <Food fav="KimChi2" />
+      <Food fav="KimChi3" />
+      <Food fav="KimChi4" />
+      <Food fav="KimChi5" />
+    </div>
+  );
 }
 
 export default App;
