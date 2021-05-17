643e4bd8bdeca52db19008345bea4d9864fd3520
Jongwoo Han <jongwoo0221@gmail.com>
2021-05-18 01:16:01 +0900

    Delete default codes from create-react-app command

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          리액트로 만드는 홈페이지
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          리액트를 배워보자
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App" />
 }
 
 export default App;
